d7791f82c3c4f7c3485ca75c4c33631a099b6ccd
Nyar233 <30363595+Nyar233@users.noreply.github.com>
2024-02-18 00:47:40 +0800

    change

--- parsers/hysteria.py
+++ parsers/hysteria.py
@@ -13,8 +13,8 @@ def parse(data):
         'type': 'hysteria',
         'server': re.sub(r"\[|\]", "", server_info.netloc.rsplit(":", 1)[0]),
         'server_port': int(server_info.netloc.rsplit(":", 1)[1]),
-        'up_mbps': int(re.search(r'\d+', netquery.get('upmbps', '')).group()),
-        'down_mbps': int(re.search(r'\d+', netquery.get('downmbps', '')).group()),
+        'up_mbps': int(re.search(r'\d+', netquery.get('upmbps', '0')).group()),
+        'down_mbps': int(re.search(r'\d+', netquery.get('downmbps', '0')).group()),
         'auth_str': netquery.get('auth', ''),
         'tls': {
             'enabled': True,
